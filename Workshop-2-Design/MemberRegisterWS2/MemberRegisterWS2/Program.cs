c60bb6a03d2c75b2c101b754a1ff7dd41cff54b8
AndréasAnemyrLNU <aa223ig@student.lnu.se>
2015-10-01 02:46:00 +0200

    Program.cs added...

---
+++ Workshop-2-Design/MemberRegisterWS2/MemberRegisterWS2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MemberRegisterWS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller.Root rootController = new Controller.Root(new View.Menu());
+            rootController.doApplication();
+        }
+    }
+}
