77aaaf0396669cd784bd7f0657a622ed0c17800c
AndréasAnemyrLNU <aa223ig@student.lnu.se>
2015-10-01 02:48:16 +0200

    Controller added also

---
+++ Workshop-2-Design/MemberRegisterWS2/MemberRegisterWS2/Controller/MemberCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegisterWS2.Controller
+{
+    class MemberCreate
+    {
+        private View.MemberCreate view;
+
+        private Model.Member member;
+        
+        public MemberCreate() 
+        {
+            //Create instance for view for interaction with client
+            view = new View.MemberCreate();
+            member = new Model.Member();
+        }
+
+        public void doMemberCreate() 
+        {
+            bool firstNameApproved = false;
+            bool lastNameApproved = false; 
+            bool persNrApproved = false;
+
+            do
+            {
+                try
+                {
+                    member.Name = view.askClientAboutFirstName();
+                    firstNameApproved = true;                
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (firstNameApproved != true && lastNameApproved != true && persNrApproved != true);
+
+            
+            
+            //string lastName = view.askClientAboutLastName();
+
+            //string persNr = view.askClientAboutPersonalNumber();
+
+
+            //return true;
+        }
+    }
+}
---
+++ Workshop-2-Design/MemberRegisterWS2/MemberRegisterWS2/Controller/Root.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegisterWS2.Controller
+{
+    class Root
+    {
+        public View.Menu Menu { get; set; }
+
+        //Constructor
+        public Root(View.Menu menu)
+        {
+            Menu = menu;
+        }
+        // This is the root controller. doApplication() could be named doInitApp().
+        public void doApplication()
+        {
+            // 1.0 Render Menu for Client
+            Menu.Welcome();
+        
+            // 2.0 Wait for Selection by user
+            do
+            {
+                // 3.0 Find out client´s selection
+                switch (Menu.clientDidChoose()) 
+                {
+                    // This is Req 1
+                    case View.Menu.MenuSelection.MemberCreate:            
+                        Controller.MemberCreate controller = new Controller.MemberCreate();
+                        controller.doMemberCreate();
+                    break;
+                    // This is Req 2 - Consists of further 2 reqs!
+                    //case View.Menu.MenuSelection.MemberReadAll:
+
+                    //break;
+                      
+                }
+            }
+            while (Menu.clientDidChoose() != View.Menu.MenuSelection.Quit);
+        }
+        
+    }
+}
