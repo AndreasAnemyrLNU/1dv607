c2d94d2257c57e27171306d14c16ff66a3d71ab8
AndréasAnemyrLNU <aa223ig@student.lnu.se>
2015-10-01 02:48:55 +0200

    Model added

---
+++ Workshop-2-Design/MemberRegisterWS2/MemberRegisterWS2/Model/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegisterWS2.Model
+{
+    class Member
+    {
+        private string name;
+       
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                if (value != "asdf") 
+                {
+                    throw new Exception("asdf");
+                }                
+                name = value;
+            }
+        }
+   
+
+        public string PersNr { get; set; }
+
+        public Member() 
+        {
+            //Empty
+        }
+
+        public Member(string name, string persNr)
+        {
+            Name = name;
+            PersNr = persNr;
+        }
+    }
+}
